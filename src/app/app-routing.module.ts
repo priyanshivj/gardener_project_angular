4d01e443729c0cd6a46b23a33fbefacb8a7eaf9b
priyanshi-vj <priyanshivijay100@gmail.com>
2024-01-31 01:40:50 +0530

    new file:   src/assets/scss/bootstrap/scss/mixins/_resize.scss 	new file:   src/assets/scss/bootstrap/scss/mixins/_table-variants.scss 	new file:   src/assets/scss/bootstrap/scss/mixins/_text-truncate.scss 	new file:   src/assets/scss/bootstrap/scss/mixins/_transition.scss 	new file:   src/assets/scss/bootstrap/scss/mixins/_utilities.scss 	new file:   src/assets/scss/bootstrap/scss/mixins/_visually-hidden.scss 	new file:   src/assets/scss/bootstrap/scss/utilities/_api.scss 	new file:   src/assets/scss/bootstrap/scss/vendor/_rfs.scss 	modified:   src/index.html 	modified:   src/main.ts 	modified:   src/styles.css 	modified:   tsconfig.app.json 	modified:   tsconfig.json 	modified:   tsconfig.spec.json

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { AboutComponent } from './Components/about/about.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { OurteamComponent } from './Components/ourteam/ourteam.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HomeComponent } from './Components/home/home.component';
+
+const routes: Routes = [
+  {path: '',
+  component: HomeComponent,
+  title: 'Home Page',
+ },
+  {path:'about', component: AboutComponent,title:'About Page'},
+  {path:'services', component: ServicesComponent,title:'Services Page'},
+  {path:'projects', component: ProjectsComponent,title:'Project Page'},
+  {path:'ourteam', component: OurteamComponent,title:'Ourteam Page'},
+  {path:'contact', component: ContactComponent,title:'Contact Page'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Components/home/home.component';
+import { AboutComponent } from './Components/about/about.component';
+import { ServicesComponent } from './Components/services/services.component';
+import { ProjectsComponent } from './Components/projects/projects.component';
+import { OurteamComponent } from './Components/ourteam/ourteam.component';
+import { ContactComponent } from './Components/contact/contact.component';
+import { HeaderComponent } from './Components/header/header.component';
+import { FooterComponent } from './Components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    AboutComponent,
+    ServicesComponent,
+    ProjectsComponent,
+    OurteamComponent,
+    ContactComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
